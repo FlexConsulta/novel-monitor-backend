4b3bffd20d7d22c3610a8b0144463f2a872f7064
Anael Medeiros <anael.medeiros@CTNAPPLE71P6QCM.local>
2022-10-01 15:27:42 -0400

    add bash file

--- src/routes/SendForgotPassword.route.js
+++ src/routes/SendForgotPassword.route.js
@@ -4,14 +4,10 @@ const router = express.Router();
 
 import SendForgotPasswordMailController from "../controllers/SendForgotPasswordMail.controller";
 
-router
-  .route("/api/passwordrecovery")
-  .get()
-  .post(
-    // fieldsValidations("authenticate"),
-    expressValidator,
-    SendForgotPasswordMailController.recovery
-  );
-
+router.route("/api/passwordrecovery").get().post(
+  // fieldsValidations("authenticate"),
+  expressValidator,
+  SendForgotPasswordMailController.recovery
+);
 
 export default router;
